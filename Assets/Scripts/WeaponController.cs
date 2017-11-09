5156b15d45d01cb85b9f4e47bc581e7ffccc749a
Ольга Кинчак < >
2017-11-09 21:05:24 +0300

    enemy ship model

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+	public Transform shotSpawn;
+	public GameObject shot;
+	private AudioSource _audioSource;
+
+	public float fireRate;
+	public float fireDelay;
+
+	// Use this for initialization
+	void Start () {
+		_audioSource = GetComponent<AudioSource> ();
+		InvokeRepeating ("Fire", fireDelay, fireRate);
+	}
+
+	private void Fire(){
+		Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		_audioSource.Play ();
+	}
+
+}
